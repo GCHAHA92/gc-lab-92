1653d5de8c5b6c6c0a5ac8b4e421bb8320ee4725
GCHAHA92 <163789849+GCHAHA92@users.noreply.github.com>
2026-08-16 19:47:06 +0900

    Remove obsolete Vercel configuration

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
-// 카카오 디벨로퍼스 > 내 애플리케이션 > 앱 키의 JavaScript 키를 입력하세요.
-// Web 플랫폼 사이트 도메인에 GitHub Pages 주소도 등록해야 합니다.
+// 카카오 디벨로퍼스 > 내 애플리케이션 > 앱 키의 JavaScript 키
+// Web 플랫폼 사이트 도메인에 GitHub Pages 주소를 등록해야 합니다.
 window.GEUMCHEON_CONFIG = {
   kakaoJavaScriptKey: 'a5e5bd3113d4570be3a6675f338f8960',
-  // Vercel에서 같은 사이트를 열면 빈 값으로 사용합니다.
-  apiBaseUrl: '',
 };
