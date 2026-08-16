a294d72965a03b19340b0359bdcd4b64aea1c588
GCHAHA92 <163789849+GCHAHA92@users.noreply.github.com>
2026-08-16 20:58:28 +0900

    Remove legacy root config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// 카카오 디벨로퍼스 > 내 애플리케이션 > 앱 키의 JavaScript 키
-// Web 플랫폼 사이트 도메인에 GitHub Pages 주소를 등록해야 합니다.
-window.GEUMCHEON_CONFIG = {
-  kakaoJavaScriptKey: 'a5e5bd3113d4570be3a6675f338f8960',
-};
