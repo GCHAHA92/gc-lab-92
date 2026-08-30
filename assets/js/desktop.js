dd5ab64ee587ce6706f8fca916e18057c3007bbe
GCHAHA92 <163789849+GCHAHA92@users.noreply.github.com>
2026-08-31 02:38:28 +0900

    style: unify desktop shortcuts across pages

--- assets/js/desktop.js
+++ assets/js/desktop.js
@@ -13,15 +13,21 @@
   shortcuts.className = 'gc-shell-shortcuts';
   shortcuts.setAttribute('aria-label', '바탕화면 바로가기');
   shortcuts.innerHTML = `
-    <button class="gc-shell-shortcut" type="button" data-gc-home>
+    <a class="gc-shell-shortcut" href="${root}tools/">
       <span class="gc-shell-shortcut-icon"><img src="${root}assets/icons/folder.svg" alt=""></span><span>MY_TOOLS</span>
-    </button>
-    <button class="gc-shell-shortcut" type="button" data-gc-guestbook>
-      <span class="gc-shell-shortcut-icon"><img src="${root}assets/icons/guestbook.svg" alt=""></span><span>방명록</span>
-    </button>
+    </a>
     <button class="gc-shell-shortcut" type="button" data-gc-message="새올행정시스템 바로가기입니다.">
       <span class="gc-shell-shortcut-icon"><img src="${root}assets/images/saeol.ico" alt=""></span><span>새올</span>
     </button>
+    <a class="gc-shell-shortcut" href="${root}about/">
+      <span class="gc-shell-shortcut-icon"><img src="${root}assets/icons/computer.svg" alt=""></span><span>ABOUT_ME</span>
+    </a>
+    <a class="gc-shell-shortcut" href="${root}notes/">
+      <span class="gc-shell-shortcut-icon"><img src="${root}assets/icons/notebook.svg" alt=""></span><span>NOTES</span>
+    </a>
+    <a class="gc-shell-shortcut" href="${root}guestbook/">
+      <span class="gc-shell-shortcut-icon"><img src="${root}assets/icons/guestbook.svg" alt=""></span><span>방명록</span>
+    </a>
     <button class="gc-shell-shortcut" type="button" data-gc-message="삭제된 아이디어도 언젠가 부활할 수 있습니다.">
       <span class="gc-shell-shortcut-icon"><img src="${root}assets/icons/trash.svg" alt=""></span><span>TRASH</span>
     </button>`;
@@ -48,12 +54,6 @@
     toastTimer = setTimeout(() => toast.classList.remove('show'), 2200);
   };
 
-  shortcuts.querySelector('[data-gc-home]').addEventListener('click', () => {
-    location.href = `${root}tools/`;
-  });
-  shortcuts.querySelector('[data-gc-guestbook]').addEventListener('click', () => {
-    location.href = `${root}guestbook/`;
-  });
   shortcuts.querySelectorAll('[data-gc-message]').forEach(button => {
     button.addEventListener('click', () => showToast(button.dataset.gcMessage));
   });
