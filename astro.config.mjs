c4b04de4d080979c875b4ba537b125d5a3db3e86
GCHAHA92 <163789849+GCHAHA92@users.noreply.github.com>
2026-08-31 22:41:54 +0900

    Support alternate GitHub Pages base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 const site = process.env.SITE_URL || 'https://nonbisa.com';
+const base = process.env.SITE_BASE || '/';
 
 export default defineConfig({
   site,
-  base: '/',
+  base,
   publicDir: './.astro-public',
   output: 'static',
   trailingSlash: 'always',
